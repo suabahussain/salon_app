65383a866ee35a12b44b1dcf7ba13b24b0655db7
Sara Abahussain <sara.abahussain@gmail.com>
2026-03-17 02:10:26 +0300

    Simplify setup.py for clean install

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
-    install_requires = f.read().strip().split("\n")
-
 setup(
-    name="salon_booking",
-    version="0.0.1",
-    description="Salon and beauty services booking platform for Frappe",
-    author="Your Name",
-    author_email="you@example.com",
+    name='salon_booking',
+    version='0.0.1',
+    description='Salon Booking App',
+    author='Sara',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=install_requires,
 )
