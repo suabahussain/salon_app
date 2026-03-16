8a12eadfe869ca4e2a6565bd20a7747b761c27c6
Sara Abahussain <sara.abahussain@gmail.com>
2026-03-16 14:12:12 +0300

    Fix setup.py import issue

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
-from setuptools import setup, find_packages
+ffrom setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-    install_requires = f.read().strip().split('\n')
-
-from salon_booking import __version__ as version
+with open("requirements.txt") as f:
+    install_requires = f.read().strip().split("\n")
 
 setup(
-    name='salon_booking',
-    version=version,
-    description='Uber-like salon and beauty services booking platform for Frappe',
-    author='Your Name',
-    author_email='you@example.com',
+    name="salon_booking",
+    version="0.0.1",
+    description="Salon and beauty services booking platform for Frappe",
+    author="Your Name",
+    author_email="you@example.com",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
